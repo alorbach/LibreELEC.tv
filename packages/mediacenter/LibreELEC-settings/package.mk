4d84d63ab0b60870e46d88fbb5d60f979647d483
alorbach <alorbach@adiscon.com>
2017-09-18 08:11:49 +0200

    removed invalid sha256

--- packages/mediacenter/LibreELEC-settings/package.mk
+++ packages/mediacenter/LibreELEC-settings/package.mk
@@ -18,7 +18,7 @@
 
 PKG_NAME="LibreELEC-settings"
 PKG_VERSION="01efc55"
-PKG_SHA256="1dc7d026f2bec6f068c8fce38e5a61efdb17bc49cf2f0e535e103b63d46a5fa4"
+#PKG_SHA256="1dc7d026f2bec6f068c8fce38e5a61efdb17bc49cf2f0e535e103b63d46a5fa4"
 PKG_ARCH="any"
 PKG_LICENSE="prop."
 PKG_SITE="https://libreelec.tv"
